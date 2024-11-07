fdece4d59f26cf653131dae2ff3a888323b18f19
victor <victorhugoornellas@gmail.com>
2024-11-07 10:17:57 -0300

    Colocando o Icone nas despesas com JS

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 // Seleciona os elementos do formulario
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Seleciona os elementos da lista
+
+const expenseList = document.querySelector("ul")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -24,4 +31,48 @@ function formatCurrencyBRL(value) {
     
     
      return value
+}
+
+    //Captura o evento de submit do formulario para obter os valores
+form.onsubmit = (event) => {
+    // Previne o comportamento padrao da pagina
+    event.preventDefault ()
+
+        // Cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date() .getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        createad_at: new Date(),
+    }
+
+   //Chama a função que irá adicionar o item na lista
+   expenseAdd(newExpense)
+}
+
+
+function expenseAdd(newExpense){
+    try {
+        //Cria o elemento para adicionar o item (li) na lista (ul)
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        // Cria o icone da categoria
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+     //Adiciona as informações no item
+
+        expenseItem.append(expenseIcon)
+
+     // Adiciona o item na lista
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        alert("Não foi possivel atulizar a lista de despesas.")
+        console.log(error)
+    }
 }
