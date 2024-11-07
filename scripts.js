6ae96e7e8b2ee5e12cc5e4b13e1cea6866a5027b
victor <victorhugoornellas@gmail.com>
2024-11-07 10:35:29 -0300

    Colocando o icone de remover com JS

--- scripts.js
+++ scripts.js
@@ -86,10 +86,16 @@ function expenseAdd(newExpense){
             .toUpperCase()
             .replace("R$", "")}`
 
+        // Adicionado o icone de remover
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
 
      //Adiciona as informações no item
 
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
      // Adiciona o item na lista
         expenseList.append(expenseItem)
